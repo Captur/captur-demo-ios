f7f584ec7f3cb212afabf6b8df181325a045eacb
ahd-3hd <ahdhani0@gmail.com>
2026-07-20 16:20:05 +0100

    Initial Commit

---
+++ CapturDemo/CapturDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapturDemoApp.swift
+//  CapturDemo
+//
+//  Created by Ahd H A Radwan on 20/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct CapturDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
